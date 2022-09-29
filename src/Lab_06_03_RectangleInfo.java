50210794d7d25639d36595979ab13e98ee1dd916
Rudi <vogelrj@mail.uc.edu>
2022-09-28 20:21:17 -0400

    updated

--- src/Lab_06_03_RectangleInfo.java
+++ src/Lab_06_03_RectangleInfo.java
@@ -15,32 +15,28 @@ public class Lab_06_03_RectangleInfo {
         System.out.println("Let's talk about a rectangle. What is the length of the horizontal sides? [in]? ");
         if (in.hasNextDouble()) {
             horizontals = in.nextDouble();
+            in.nextLine();
         } else {
             trash.equals(in.nextLine());
-            System.out.println("Invalid Entry");
+            System.out.println("Invalid Entry. Please Try Again.");
+            System.exit(0);
         }
         System.out.println("What is the length of the vertical sides? [in]? ");
         if (in.hasNextDouble()) {
             verticals = in.nextDouble();
+            in.nextLine();
         }
         else {
             trash.equals(in.nextLine());
-            System.out.println("Invalid Entry");
-        }
-        if (verticals == horizontals){
-            System.out.println("Are you sure this is a rectangle?");
+            System.out.println("Invalid Entry. Please Try Again.");
+            System.exit(0);;
         }
         rectPeri = (horizontals + verticals) * 2;
         areaRect = horizontals * verticals;
         diagWork = (Math.pow(horizontals, 2)) + (Math.pow(verticals, 2));
         diagRect = Math.sqrt(diagWork);
-        if (rectPeri ==0 || areaRect ==0 || diagRect ==0){
-            System.out.println("Please Try Again.");
-        }
-        else{
             System.out.println("The perimeter of the rectangle is " + df.format(rectPeri) + " inches.");
             System.out.println("the area of the rectangle is " + df.format(areaRect) + " inches.");
             System.out.println("The diagonal inside the rectangle is " + df.format(diagRect) + " inches.");
-        }
     }
 }
